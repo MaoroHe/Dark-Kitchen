48b69e8b6dc108b87c5c484c61dbd94ab3dad855
Aurelien229 <blampainaurelien229@gmail.com>
2023-12-18 13:59:56 +0100

    aurelien

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const body = document.body;
+
+    darkModeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+    });
+});
+
+
 const menu = [
     {
         name: "Margherita",
@@ -73,9 +83,9 @@ function cardCreator() {
 
 
 function validateForm() {
-    var customerName = document.getElementById('customerName').value;
-    var customerAddress = document.getElementById('customerAddress').value;
-    var items = document.getElementById('items').value;
+    let customerName = document.getElementById('customerName').value;
+    let customerAddress = document.getElementById('customerAddress').value;
+    let items = document.getElementById('items').value;
 
     if (customerName === "" || customerAddress === "" || items === "") {
         alert("Veuillez remplir tous les champs du formulaire.");
@@ -83,4 +93,33 @@ function validateForm() {
     }
 
     return true;
+}
+
+//
+
+let currentIndex = 0;
+
+function showSlide(index) {
+  const carousel = document.querySelector('.carousel');
+  const slides = document.querySelectorAll('.item');
+  const totalSlides = slides.length;
+
+  if (index >= totalSlides) {
+    currentIndex = 0;
+  } else if (index < 0) {
+    currentIndex = totalSlides - 1;
+  } else {
+    currentIndex = index;
+  }
+
+  const translateValue = -currentIndex * 100 + '%';
+  carousel.style.transform = 'translateX(' + translateValue + ')';
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
 }
