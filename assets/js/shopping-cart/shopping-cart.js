ce39bb8fd434eee60ec9e09f9d105e9599bc2025
MaoroHe <maorohecq1409@gmail.com>
2023-12-20 14:24:10 +0100

    update

--- assets/js/shopping-cart/shopping-cart.js
+++ assets/js/shopping-cart/shopping-cart.js
@@ -13,23 +13,50 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// function qui ecoute les clics et ajoute la troisieme classe du boutton cliquer qui est egale au nom de la pizza pour la display dans l'ul du panier.
+//
 
 const addToCartButton = document.querySelectorAll('button.paye');
 const lesul = document.querySelector('ul.item');
+const panier = [];
 
 const clickOnCart = (e) => {
-    let liCreator = document.createElement('li')
-    let commandText = document.createTextNode(e.target.classList[2] + ' est dans votre panier en ' + '' + 'exemplaire(s) !');
-    let trieur = e.target.classList[2];
+    const article = e.target.classList[2];
 
-    
-        lesul.appendChild(liCreator)
-        liCreator.appendChild(commandText)
-    
+    // Vérifier si l'article est déjà dans le panier
+    const existingItemIndex = panier.findIndex(item => item.article === article);
 
+    if (existingItemIndex !== -1) {
+        // Si l'article est déjà dans le panier, augmenter ou diminuer la quantité
+        const newQuantity = panier[existingItemIndex].quantity + 1;
+        panier[existingItemIndex].quantity = newQuantity;
+
+    } else {
+        // Si l'article n'est pas dans le panier, l'ajouter avec une quantité de 1
+        const newItem = { article, quantity: 1 };
+        panier.push(newItem);
+    }
+
+    // Mettre à jour l'affichage du panier
+    afficherPanier();
+}
+
+// Afficher le contenu du panier
+function afficherPanier() {
+    // Effacer le contenu actuel du panier
+    lesul.innerHTML = '';
+
+    // Afficher chaque article dans le panier
+    panier.forEach(item => {
+        let liCreator = document.createElement('li');
+        let commandText = document.createTextNode(`${item.article} est dans votre panier en ${item.quantity} exemplaire(s) !`);
+        liCreator.className = `${item.article} li`;
+
+        lesul.appendChild(liCreator);
+        liCreator.appendChild(commandText);
+        liCreator.className = item.article;
+    });
 }
 
 for (let actionSquare of addToCartButton) {
-    actionSquare.addEventListener('click', clickOnCart)
-  }
+    actionSquare.addEventListener('click', clickOnCart);
+}
